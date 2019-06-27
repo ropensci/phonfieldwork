fe506c7047c4fac930ae60c92737835f13241eb0
George Moroz <agricolamz@gmail.com>
2019-06-27 17:13:51 +0300

    correct prezi function

--- R/create_presentation.R
+++ R/create_presentation.R
@@ -36,5 +36,5 @@ create_presentation <- function(stimuli,
                collapse = "")
   writeLines(rmd, paste0(filename, ".Rmd"))
   rmarkdown::render(paste0(filename, ".Rmd"))
-  file.remove("stimuli_prezi.Rmd")
+  file.remove(paste0(filename, ".Rmd"))
 }
