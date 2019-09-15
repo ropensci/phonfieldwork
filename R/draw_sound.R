76b43b45c756af8b4b58ce7f1b8859f63d58be73
George Moroz <agricolamz@gmail.com>
2019-09-15 19:17:51 +0300

    make draw_sound() work with folder of files

--- R/draw_sound.R
+++ R/draw_sound.R
@@ -33,56 +33,100 @@ draw_sound <- function(file_name,
                        window_length = 5,
                        output_width = 750,
                        output_height = 500,
-                       output_units = "px"){
-  if(is.null(output_file)){
+                       output_units = "px",
+                       sounds_from_folder = NULL,
+                       pic_folder_name = "pics"){
+  if(is.null(sounds_from_folder)){
+    if(is.null(output_file)){
 # read file and convert to phonTools format -------------------------------
-    s <- tuneR::readWave(file_name)
-    sound <- phonTools::makesound(s@left, fs = s@samp.rate)
+      s <- tuneR::readWave(file_name)
+      sound <- phonTools::makesound(s@left, fs = s@samp.rate)
 
 # if there is no title no need to have a space for it ---------------------
-    title_space <- ifelse(is.null(title), 0, 2)
+      title_space <- ifelse(is.null(title), 0, 2)
 
 # set viewer parameters ---------------------------------------------------
-    par(oma=c(0,0,title_space,0), mai=c(0, 0, 0, 0), fig=c(0.05,1,0.8,1))
+      par(oma=c(0,0,title_space,0), mai=c(0, 0, 0, 0), fig=c(0.05,1,0.8,1))
 
 # plot oscilogram ---------------------------------------------------------
-    tuneR::plot(s, xlab = "", las=0, ann = FALSE, xaxt = "n", main = title)
-    title(title, outer = TRUE)
-    axis(1, at=1:round(sound$duration/100)/10,labels=1:round(sound$duration/100)*100,
-         las=1)
+      tuneR::plot(s, xlab = "", las=0, ann = FALSE, xaxt = "n", main = title)
+      title(title, outer = TRUE)
+      axis(1, at=1:round(sound$duration/100)/10,labels=1:round(sound$duration/100)*100,
+           las=1)
 
 # plot spectrogram --------------------------------------------------------
-    par(fig=c(0.05,1,0.08,0.72), new=TRUE, las=0)
-    phonTools::spectrogram(sound,
-                           windowlength = window_length,
-                           colors = spectrum_colors,
-                           maxfreq = maximum_frequency,
-                           dynamicrange = dynamic_range)
-    axis(1, at=1:round(sound$duration/100)*100,labels=1:round(sound$duration/100)*100,
-         las=1)
+      par(fig=c(0.05,1,0.08,0.72), new=TRUE, las=0)
+      phonTools::spectrogram(sound,
+                             windowlength = window_length,
+                             colors = spectrum_colors,
+                             maxfreq = maximum_frequency,
+                             dynamicrange = dynamic_range)
+      axis(1, at=1:round(sound$duration/100)*100,labels=1:round(sound$duration/100)*100,
+           las=1)
 
 # reset graphical parameters to default -----------------------------------
-    par(oma=c(0,0,0,0), mai=c(1.02, 0.82, 0.82, 0.42), fig=c(0,1,0,1))
+      par(oma=c(0,0,0,0), mai=c(1.02, 0.82, 0.82, 0.42), fig=c(0,1,0,1))
 
-  } else {
+    } else {
 
 # save a file -------------------------------------------------------------
-    grDevices::png(filename = paste0(output_file, ".png"),
-        width = output_width,
-        height = output_height,
-        units = output_units)
-    draw_sound(file_name,
-               title,
-               spectrum_colors,
-               maximum_frequency,
-               dynamic_range,
-               window_length,
-               output_file = NULL)
-    grDevices::dev.off()
+      grDevices::png(filename = paste0(output_file, ".png"),
+          width = output_width,
+          height = output_height,
+          units = output_units)
+      draw_sound(file_name,
+                 title,
+                 spectrum_colors,
+                 maximum_frequency,
+                 dynamic_range,
+                 window_length,
+                 output_file = NULL)
+      grDevices::dev.off()
+    }
+
+# in case of multuple files -----------------------------------------------
+  } else {
+# get a correct picture folder path ---------------------------------------
+    sounds_from_folder <- normalizePath(sounds_from_folder)
+    list.files(sounds_from_folder)
+    slashes <- unlist(gregexpr("/", sounds_from_folder))
+    cut <- unlist(gregexpr("/", sounds_from_folder))[length(slashes)]
+    pic_path <- paste0(substr(sounds_from_folder, 1, cut), pic_folder_name)
+
+# create a directory if it is not exists ----------------------------------
+    if(!dir.exists(pic_path)){
+      dir.create(pic_path)
+    }
+
+# get list of sounds and future pictures ----------------------------------
+    sounds <- paste0(sounds_from_folder, "/", list.files(sounds_from_folder))
+
+    unlist(
+      lapply(seq_along(sounds), function(i){
+        res <- unlist(strsplit(sounds[i], "\\."))
+        res[length(res)]
+      })) ->
+      extension
+    files_from_list <- which(tolower(extension) %in% "wav")
+
+    sounds <- sounds[files_from_list]
+
+    pics <- paste0(pic_path, "/",
+                   list.files(sounds_from_folder)[files_from_list])
+    pics <- substr(pics, 1, nchar(pics)-4)
+
+# loop over the draw_sound function ---------------------------------------
+    lapply(seq_along(sounds), function(i){
+      draw_sound(sounds[i],
+                 output_file = pics[i],
+                 title = list.files(sounds_from_folder)[files_from_list][i],
+                 spectrum_colors = spectrum_colors,
+                 maximum_frequency = maximum_frequency,
+                 dynamic_range = dynamic_range,
+                 window_length = window_length,
+                 output_width = output_width,
+                 output_height = output_height,
+                 output_units = output_units)
+    })
   }
 }
-
-
-
-
-
