906411c60a2581c1618a680ce560a6df480495c2
George Moroz <agricolamz@gmail.com>
2020-07-18 20:47:33 +0300

    add some tests

---
+++ tests/testthat/test-create-glossed-document.R
@@ -0,0 +1,17 @@
+library(phonfieldwork)
+context("Tests for 'create_glossed_document()' functions")
+
+my_tmp1 <- tempdir()
+create_glossed_document("../../vignettes/files/zilo_test.flextext",
+                        output_dir = my_tmp1)
+
+my_tmp2 <- tempdir()
+create_glossed_document(
+  flextext_to_df("../../vignettes/files/zilo_test.flextext"),
+  output_dir = my_tmp2)
+
+test_that("create_glossed_document", {
+  expect_true(file.exists(paste0(my_tmp1, "/glossed_document.docx")))
+  expect_true(file.exists(paste0(my_tmp2, "/glossed_document.docx")))
+})
+
