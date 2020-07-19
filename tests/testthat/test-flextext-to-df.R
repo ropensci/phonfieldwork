8706d1e371bbc647b1a885768aff47a5032b60f2
George Moroz <agricolamz@gmail.com>
2020-07-19 11:21:17 +0300

    correct travis

--- tests/testthat/test-flextext-to-df.R
+++ tests/testthat/test-flextext-to-df.R
@@ -1,6 +1,6 @@
 library(phonfieldwork)
 context("Tests for 'flextext_to_df()' functions")
-file_name <- "../../vignettes/files/zilo_test.flextext"
+file_name <- "https://raw.githubusercontent.com/agricolamz/phonfieldwork/master/vignettes/files/zilo_test.flextext"
 df <- flextext_to_df(file_name)
 test_that("flextext_to_df", {
   expect_true(nrow(df) == 1170)
