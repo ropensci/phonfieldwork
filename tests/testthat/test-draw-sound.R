11517a79cdb5b50eb050fb9fe14bf54352083e72
George Moroz <agricolamz@gmail.com>
2020-07-19 14:35:50 +0300

    add tests

--- tests/testthat/test-draw-sound.R
+++ tests/testthat/test-draw-sound.R
@@ -18,10 +18,29 @@ draw_sound(system.file("extdata", "test.wav", package = "phonfieldwork"),
            pitch = system.file("extdata", "test.Pitch", package = "phonfieldwork"))
 supress_message <- grDevices::dev.off()
 
+grDevices::png(filename = paste0(tdir, "/draw_sound/test4.png"))
+draw_sound(system.file("extdata", "test.wav", package = "phonfieldwork"),
+           pitch = system.file("extdata", "test.Pitch", package = "phonfieldwork"))
+supress_message <- grDevices::dev.off()
+
+grDevices::png(filename = paste0(tdir, "/draw_sound/test5.png"))
+draw_sound(system.file("extdata", "test.wav", package = "phonfieldwork"),
+           system.file("extdata", "test.TextGrid", package = "phonfieldwork"),
+           zoom = c(0.4, 0.65),
+           pitch = system.file("extdata", "test.Pitch", package = "phonfieldwork"))
+supress_message <- grDevices::dev.off()
+
+draw_sound(system.file("extdata", "test.wav", package = "phonfieldwork"),
+           system.file("extdata", "test.TextGrid", package = "phonfieldwork"),
+           zoom = c(0.4, 0.65),
+           pitch = system.file("extdata", "test.Pitch", package = "phonfieldwork"),
+           output_file = paste0(tdir, "/draw_sound/test6"))
+
 list.files()
 test_that("draw_sound", {
   expect_equal(list.files(paste0(tdir, "/draw_sound"), "\\.png$"),
-               c("test1.png", "test2.png", "test3.png"))
+               c("test1.png", "test2.png", "test3.png", "test4.png",
+                 "test5.png", "test6.png"))
 })
 unlink(paste0(tdir, "/draw_sound"))
 rm(tdir)
--- tests/testthat/test-draw-spectrogram.R
+++ tests/testthat/test-draw-spectrogram.R
@@ -6,10 +6,18 @@ grDevices::png(filename = paste0(tdir, "/draw_spectrogram/test1.png"))
 draw_spectrogram(system.file("extdata", "test.wav", package = "phonfieldwork"))
 supress_message <- grDevices::dev.off()
 
-list.files()
+raven_an <- data.frame(time_start = 450,
+                       time_end  = 520,
+                       freq_low = 3,
+                       freq_high = 4.5)
+grDevices::png(filename = paste0(tdir, "/draw_spectrogram/test2.png"))
+draw_sound(system.file("extdata", "test.wav", package = "phonfieldwork"),
+           raven_annotation = raven_an)
+supress_message <- grDevices::dev.off()
+
 test_that("draw_spectrogram", {
   expect_equal(list.files(paste0(tdir, "/draw_spectrogram"), "\\.png$"),
-               c("test1.png"))
+               c("test1.png", "test2.png"))
 })
 unlink(paste0(tdir, "/draw_spectrogram"))
 rm(tdir)
