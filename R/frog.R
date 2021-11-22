36a79a738c826b92744f0049f3e356245ba21a93
vbunt <54185665+vbunt@users.noreply.github.com>
2021-11-22 11:26:19 +0300

    Update frog.R

--- R/frog.R
+++ R/frog.R
@@ -2,9 +2,16 @@
 #
 # Returns a string of 'kva'
 #
+# @author Valeria Buntiakova <valleriabun@gmail.com>
+#
 # @param length of string
 #
 # @return a string of 'kva'
+#
+# @examples
+# frog(3)
+# 
+# @export
 #
 
 frog <- function(n){
