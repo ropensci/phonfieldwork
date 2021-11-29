310e1e356e7d9ce794a5de47db984242a6b3d7a2
vbunt <54185665+vbunt@users.noreply.github.com>
2021-11-29 09:48:22 +0300

    Update frog.R

--- R/frog.R
+++ R/frog.R
@@ -1,20 +1,19 @@
-# frog
-#
-# Returns a string of 'kva'
-#
-# @author Valeria Buntiakova <valleriabun@gmail.com>
-#
-# @param length of string
-#
-# @return a string of 'kva'
-#
-# @examples
-# frog(3)
-# 
-# @export
-#
+#' frog
+#'
+#' Returns a string of 'kva'
+#'
+#' @author Valeria Buntiakova <valleriabun@gmail.com>
+#'
+#' @param length of string
+#'
+#' @return a string of 'kva'
+#'
+#' @examples
+#' frog(3)
+#' 
+#' @export
+#'
 
 frog <- function(n){
-  kvas = rep('kva', n)
-  return(paste(kvas, collapse='-'))
+  paste(rep('kva', n), collapse='-')
 }
